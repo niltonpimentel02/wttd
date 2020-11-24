ab30a737e89824aa444aec8d65de762d31103377
Nilton <niltonpimentel02@gmail.com>
2020-11-24 13:49:43 -0300

    From alinhado com o Sender Identity

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -17,8 +17,8 @@ def subscribe(request):
             mail.send_mail(
                 'Confirmação de inscrição',
                 body,
-                'contato@eventex.com.br',
-                ['contato@eventex.com.br', form.cleaned_data['email']]
+                'contato@niltonpimentel.com.br',
+                ['contato@niltonpimentel.com.br', form.cleaned_data['email']]
             )
 
             messages.success(request, 'Inscrição realizada com sucesso!')
